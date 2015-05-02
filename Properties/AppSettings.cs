b0fa7ed97de55e941626854e2feb588aadbc9fef
Babis Karypidis <xabikos@hotmail.com>
2015-05-02 17:10:35 +0300

    Create the default asp.net vnext MVC application

---
+++ Properties/AppSettings.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspvnextwebpack
+{
+    public class AppSettings
+    {
+        public string SiteTitle { get; set; }
+    }
+}
